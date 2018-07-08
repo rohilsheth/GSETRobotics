54df53592f08ad01d38795f01f4b315746de0a59
Lev-Stambler <levstamb@gmail.com>
2018-07-08 18:03:53 -0400

    Add files via upload

--- LineFollow.py
+++ LineFollow.py
@@ -8,10 +8,10 @@ lightSensor = LightSensor()
 lineEdgeVal = 470 #wanted area
 motorLeft = LargeMotor('outB')
 motorRight = LargeMotor('outC')
+LeftToRight = False
 
-
-lineTolerance = 40 #+- 20
-commonSpeed = 300
+lineTolerance = 20 #+- 20
+commonSpeed = 350
 rightSpeed = commonSpeed
 leftSpeed = commonSpeed
 lcd = Screen()
@@ -29,31 +29,43 @@ def runMotor(speedLeft, speedRight):
     motorRight.run_forever(speed_sp= min(int(speedRight), 900))
     
 def followLine():
+    counterAdderLeft = 4
+    counterAdderRight = 4
     sensorVal = lightSensor.value()
-    changeScalarRight = 5
-    changeScalarLeft = 5
+    changeScalarRight = 11
+    changeScalarLeft = 11
     leftCounter = 1
     rightCounter = 1
+    global onLeft
     global commonSpeed
     global leftSpeed
     global rightSpeed
     global lineEdgeVal
 
     if  sensorVal > (lineEdgeVal + lineTolerance):
+        LeftToRight = True
         rightSpeed += int((((abs(lineEdgeVal - sensorVal))/changeScalarRight))/rightCounter)
+        # rightSpeed = 450
         print("right speed: " + str(rightSpeed))
-        leftSpeed = 10
-        rightCounter += .5
+        leftSpeed = -20#100
+        rightCounter += counterAdderRight
         leftCounter = 1
+        
+
     # turn right?
     elif sensorVal < (lineEdgeVal - lineTolerance):
     # turn left?
+        # leftSpeed = 450
+        LeftToRight = False
+        onLeft = False
         leftSpeed += int((((abs(lineEdgeVal - sensorVal))/changeScalarLeft))/leftCounter)
         print("left speed: " + str(leftSpeed))
-        rightSpeed = 10
-        leftCounter += .5
+        rightSpeed = -20
+        leftCounter += counterAdderLeft
         rightCounter = 1
     else:
+        LeftToRight = False
+        onLeft = True
         leftSpeed = commonSpeed
         rightSpeed = commonSpeed
         leftCounter = 1
@@ -66,9 +78,9 @@ def followLine():
 
 def main(shutoff):
     while True:
-        lcd.draw.text((48,13), str(lightSensor.value()))
+        lcd.draw.text((48,13), str(leftSpeed) + ", " + str(rightSpeed))
         lcd.update()
         lcd.clear()
         followLine()
-        sleep(.05)
+        sleep(.01)
 main(1000)
