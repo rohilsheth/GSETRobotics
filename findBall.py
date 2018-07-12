8ee7df87feb3cb84741f3854d758768b38b816f4
abhishekp106 <40919306+abhishekp106@users.noreply.github.com>
2018-07-11 20:44:15 -0400

    Add files via upload

--- findBall.py
+++ findBall.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 from ev3dev.ev3 import *
 from time import sleep
@@ -13,45 +14,38 @@ def getBall():
 def findBall():
     sleep(1)
 def turn90Right():
-    motorLeft.run_to_rel_pos(position_sp=460, speed_sp=900, stop_action="hold")
+    motorLeft.run_to_rel_pos(position_sp=550, speed_sp=900, stop_action="hold")
 def turn90Left():
-    motorRight.run_to_rel_pos(position_sp=460, speed_sp=900, stop_action="hold")
+    motorRight.run_to_rel_pos(position_sp=550, speed_sp=900, stop_action="hold")
+def goTillTarget(targetinput, leftOrRight):
+    target = targetinput
+    us = UltrasonicSensor()
+    us.mode = 'US-DIST-CM'
+    while True:
+        dist = us.value()
+        if abs(dist - target) <= 30:
+            motorLeft.stop(stop_action='hold')
+            motorRight.stop(stop_action='hold')
+            if leftOrRight == "left":
+                turn90Left()
+            if leftOrRight == "right":
+                turn90Right()
+            break
+        elif dist > target:
+            motorRight.run_forever(speed_sp=min(300, 3 * (dist - target)))
+            motorLeft.run_forever(speed_sp=min(300, 3 * (dist - target)))
+        else:
+            motorRight.run_forever(speed_sp=max(-300, 3 * (dist - target)))
+            motorLeft.run_forever(speed_sp=max(-300, 3 * (dist - target)))
 
-target = 450
-us = UltrasonicSensor()
-us.mode='US-DIST-CM'
 
-while True:
-  dist=us.value()
-  if abs(dist-target) <= 30:
-     motorLeft.stop(stop_action='hold')
-     motorRight.stop(stop_action='hold')
-     turn90Right()
-     break
-  elif dist > target:
-     motorRight.run_forever(speed_sp=min(300,3*(dist-target)))
-     motorLeft.run_forever(speed_sp=min(300,3*(dist-target)))
-  else:
-     motorRight.run_forever(speed_sp=max(-300,3*(dist-target)))
-     motorLeft.run_forever(speed_sp=max(-300,3*(dist-target)))
+
+goTillTarget(70, "left")
 
 sleep(0.5)
 
-target = 100
+goTillTarget(380,"right")
 
-while True:
-  dist=us.value()
-  if abs(dist-target) <= 30:
-     motorLeft.stop(stop_action='hold')
-     motorRight.stop(stop_action='hold')
-     turn90Right()
-     break
-  elif dist > target:
-     motorRight.run_forever(speed_sp=min(300,3*(dist-target)))
-     motorLeft.run_forever(speed_sp=min(300,3*(dist-target)))
-  else:
-     motorRight.run_forever(speed_sp=max(-300,3*(dist-target)))
-     motorLeft.run_forever(speed_sp=max(-300,3*(dist-target)))
 
 sleep(0.5)
 
@@ -63,13 +57,20 @@ motorLeft.run_forever(speed_sp=300)
 motorLeft.stop(stop_action="break")
 motorRight.stop(stop_action="break")
 
-ir = InfraredSensor()
-ir.mode = 'IR-PROX'
 
-target = 20
+
+ir=Sensor(address='in4:i2c8',driver_name='ht-nxt-ir-seek-v2')
+ir.mode='AC'
 
 while True:
-    distance = ir.value()
-    if abs(distance-target) <= 5:
-        Sound.beep()
+    direction = ir.value()
+    if direction < 5:
+        motorLeft.run_to_rel_pos(position_sp=100, speed_sp=900, stop_action="hold")
+    if direction > 5:
+        motorRight.run_to_rel_pos(position_sp=100, speed_sp=900, stop_action="hold")
+    if direction = 5:
+        break
+
+
+
 
