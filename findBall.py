a6633559fb14ffe3dc494ebe4be24e5581ca98c8
abhishekp106 <40919306+abhishekp106@users.noreply.github.com>
2018-07-15 15:52:52 -0400

    Add files via upload

--- findBall.py
+++ findBall.py
@@ -6,7 +6,7 @@ from time import sleep
 motorLeft = LargeMotor('outC')
 motorRight = LargeMotor('outB')
 
-#hello
+#if color = red then boolean = true
 
 def getBall():
     sleep(1)
@@ -37,39 +37,48 @@ def goTillTarget(targetinput, leftOrRight):
         else:
             motorRight.run_forever(speed_sp=max(-300, 3 * (dist - target)))
             motorLeft.run_forever(speed_sp=max(-300, 3 * (dist - target)))
+def findIRBall()
+    ir = Sensor(address='in3:i2c8', driver_name='ht-nxt-ir-seek-v2')
+    ir.mode = 'AC'
+    direction = ir.value()
+    if (direction == 5) or (direction == 4) or (direction == 6):
+        # play around with this value
+        target = 40
+        while True:
+            dist = us.value()
+            if abs(dist - target) <= 30:
+                motorLeft.stop(stop_action='hold')
+                motorRight.stop(stop_action='hold')
+                sleep(0.5)
+                break
+            elif dist > target:
+                motorRight.run_forever(speed_sp=min(300, 3 * (dist - target)))
+                motorLeft.run_forever(speed_sp=min(300, 3 * (dist - target)))
+            else:
+                motorRight.run_forever(speed_sp=max(-300, 3 * (dist - target)))
+                motorLeft.run_forever(speed_sp=max(-300, 3 * (dist - target)))
 
+def align():
+    motorRight.run_to_rel_pos(position_sp=600, speed_sp=200, stop_action="hold")
+    motorLeft.run_to_rel_pos(position_sp=600, speed_sp=200, stop_action="hold")
+    sleep(0.5)
 
+    motorRight.run_to_rel_pos(position_sp=-2000, speed_sp=700, stop_action="hold")
+    motorLeft.run_to_rel_pos(position_sp=-2000, speed_sp=700, stop_action="hold")
+    sleep(0.5)
+
+align()
 
 goTillTarget(70, "left")
 
 sleep(0.5)
 
-goTillTarget(380,"right")
-
+goTillTarget(70, "right")
 
 sleep(0.5)
 
-#room2 IR scan
+findIRBall()
 
-motorRight.run_forever(speed_sp=300)
-motorLeft.run_forever(speed_sp=300)
-
-motorLeft.stop(stop_action="break")
-motorRight.stop(stop_action="break")
-
-
-
-ir=Sensor(address='in4:i2c8',driver_name='ht-nxt-ir-seek-v2')
-ir.mode='AC'
-
-while True:
-    direction = ir.value()
-    if direction < 5:
-        motorLeft.run_to_rel_pos(position_sp=100, speed_sp=900, stop_action="hold")
-    if direction > 5:
-        motorRight.run_to_rel_pos(position_sp=100, speed_sp=900, stop_action="hold")
-    if direction = 5:
-        break
 
 
 
